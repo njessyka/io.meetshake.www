e9e38a578c3bc48548e3ea2d717bfd0712b462f9
njessyka <njessyka@gmail.com>
2015-06-04 20:39:59 +0200

    Markers for geolocation

--- www/js/meet.js
+++ www/js/meet.js
@@ -1,33 +1,43 @@
 $(function(){
 
-	if (navigator.geolocation) { 
-		navigator.geolocation.getCurrentPosition(afficher_carte); 
-	} 
-	else { 
-		alert("Désolé, votre navigateur ne supporte pas la géolocalisation"); 
-	} 
+  var locations = [
+     	[ 'Marina', 48.796778, 2.310031,3],
+			[ 'Pauline', 48.792364, 2.312713,2],
+			[ 'Sandra',48.793350, 2.303604,1]
+    ];
+    
+    
+    var map = new google.maps.Map(document.getElementById('map_canvas'), {
+      zoom: 13,
+      center: new google.maps.LatLng(48.79638, 2.31159),
+      mapTypeControl: false,
+      scaleControl: true,
+    streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+ 
 
-	function afficher_carte(position){ 
-		var latitude = position.coords.latitude; 
-		var longitude= position.coords.longitude; 
-		// création d’une nouvelle carte par Google Maps s’effectue simplement par le code suivant auquel on fournit comme paramètres la latitude et la longitude trouvées par l’API de géolocalisation. 
-		var latlng = new google.maps.LatLng(latitude, longitude); 
-		// configuration d'un tableau contenant les paramètres 
-		 var myOptions = { 
-					             zoom: 15, 
-					             center: latlng, 
-					             mapTypeId: google.maps.MapTypeId.ROADMAP 
-									};    
-		var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
-		var marker = new google.maps.Marker({ 
-		             position: latlng,  
-		             map: map,  
-		             title:"Vous êtes ici"  
-		}); 
-	}
+  var infowindow = new google.maps.InfoWindow();
+ 
+  var i;
+ 
+  for (i = 0 ; i < locations.length ; i++) {  
+      var latit = locations[i][1];
+      var longit = locations[i][2];
+  
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latit, longit),
+        map: map,
+        title: locations[i][0]
+      });
+ 
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+          };
+      })(marker, i));
+    }
+  
 
-
-
-
-	
 });
