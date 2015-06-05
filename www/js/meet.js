575aa359d164c80512c8428a2c5b63b9b17ce162
Mika <webdeveloper@diw.fr>
2015-06-05 15:42:10 +0200

    corrections on meet.js file

--- www/js/meet.js
+++ www/js/meet.js
@@ -1,45 +1,45 @@
 $(function(){
-	
-	
+
+
 	var users = [
-     	[3, 'Marina','img/users/marina_3/marina_3_profile.jpg',24],
+			[3, 'Marina','img/users/marina_3/marina_3_profile.jpg',24],
 			[2, 'Pauline','img/users/pauline_2/pauline_2_profile.jpg',19],
 			[ 1,'Sandra','img/users/sandra_1/sandra_1_profile.jpg',28]
-    ];
+	];
 
   var locations = [
-     	[ 'Marina', 48.796778, 2.310031,3],
+			[ 'Marina', 48.796778, 2.310031,3],
 			[ 'Pauline', 48.792364, 2.312713,2],
 			[ 'Sandra',48.793350, 2.303604,1]
-    ];
+	];
     
     
     var map = new google.maps.Map(document.getElementById('map_canvas'), {
-      zoom: 13,
-      center: new google.maps.LatLng(48.79638, 2.31159),
-      mapTypeControl: false,
-      scaleControl: true,
-	streetViewControl: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
- 
+		zoom: 13,
+		center: new google.maps.LatLng(48.79638, 2.31159),
+		mapTypeControl: false,
+		scaleControl: true,
+		streetViewControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
 
   var infowindow = new google.maps.InfoWindow();
- 
+
   var i;
 
-  for (i = 0 ; i < locations.length ; i++) {  
+  for (i = 0 ; i < locations.length ; i++) {
       var latit = locations[i][1];
       var longit = locations[i][2];
 	  var currentId = locations[i][3];
   
       var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(latit, longit),
-        map: map,
-        title: locations[i][0]
-      });
- 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			position: new google.maps.LatLng(latit, longit),
+			map: map,
+			title: locations[i][0]
+		});
+
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
 
 		var imgUrl, user, age, content ,idUser, prenom;
 
@@ -60,11 +60,9 @@ $(function(){
 	
           return function() {
 			// window.location.replace("profile_view.php?idReq="+ currentId);
-            infowindow.setContent(content);
-            infowindow.open(map, marker);
-          };
-      })(marker, i));
-    }
-  
-
+			infowindow.setContent(content);
+			infowindow.open(map, marker);
+		};
+		})(marker, i));
+	}
 });
