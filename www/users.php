9661c631bba590a87de436d815badca9bb4935f7
njessyka <njessyka@gmail.com>
2015-06-05 11:54:10 +0200

    Session var modified

--- www/users.php
+++ www/users.php
@@ -1,7 +1,7 @@
 <?php
 $_SESSION['users']= array(
-	1 => 'Sandra',
-	2 => 'Pauline',
-	3 => 'Marina',
+	1 => array('Sandra', 'img/users/sandra_1/'),
+	2 => array('Pauline','img/users/pauline_2/' ), 
+	3 => array('Marina', 'img/users/marina_3/'),
 );
 ?>
