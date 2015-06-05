54aa611a3e6c7fb975574ce0d7717000fefea86c
Mika <webdeveloper@diw.fr>
2015-06-05 14:54:14 +0200

    modifies index.php and users.php - creates functions.php

--- www/users.php
+++ www/users.php
@@ -1,8 +1,8 @@
 <?php
-$_SESSION['users']= array(
+$_SESSION['users']= [
 	1 => array('Sandra', 'img/users/sandra_1/'),
 	2 => array('Pauline','img/users/pauline_2/' ), 
 	3 => array('Marina', 'img/users/marina_3/'),
-);
+]
 
 ?>
